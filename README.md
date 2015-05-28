c950b213eaa385196ec09ced252ad7ea7d0118c1
Alex Gondek <alexgondek@gmail.com>
2015-05-28 12:38:21 -0700

    Added project boilerplate

---
+++ README.md
@@ -0,0 +1,4 @@
+google-calendar-mailer
+======================
+
+Emails calendar events that match certain criteria.
---
+++ app.js
@@ -0,0 +1,4 @@
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var calendar = google.calendar('v3');
